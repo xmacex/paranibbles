83d1c279c51b183f08b64a24a6aa9149a6770bf9
Mace Ojala <mace.ojala@gmail.com>
2024-07-24 21:49:33 +0100

    First version, somehow works.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Paranibbles
+
+Paranibbles for paradiddles.
+
+A four step binary pattern is run, then inverted, and run again. That is eight steps. Default 3 in binary `0100 1011`.
+
+Set where to send and what to send in params.
+
+At this stage does not send MIDI off messages, so stuck notes to be expected. Use percussion sounds such as one-off drum samples.
+
+Glück auf!
+
+## inspiration
+
+Inspired by this video from the ever awesome Oskar https://www.youtube.com/watch?v=EZBpxkxJg0o
+
+## installation
+
+`;install https://github.com/xmacex/paranibbles`
+
+## name
+
+A "nibble" is half of a byte... 4 bits. Paradiddles are these things in drumming. Therefore: paranibble.
---
+++ paranibbles.lua
@@ -0,0 +1,139 @@
+-- paranibbles
+--
+-- 0100 1011
+--
+-- by xmacex
+-- inspired by oskar's video
+-- on paradiddles
+-- https://www.youtube.com/watch?v=EZBpxkxJg0o
+
+er = require 'er'
+s  = require 'sequins'
+
+
+if not tab then
+   tab = require("tab")
+end
+
+screen.width = 128
+screen.height = 64
+
+nibble  = 4
+giggle  = nil
+accents = nil
+accent  = false
+current_value = "000"
+
+function init()
+   init_params()
+   giggle  = s(nibbleToParadiddle(params:get('nibble')))
+   accents = s(er.gen(3, 8))
+
+   params:set('clock_tempo', 145)
+
+   midi_dev = midi.connect(params:get('midi_dev'))
+
+   -- redraw_clock = clock.run(redraw)
+   ui_clock = clock.run(tick)
+end
+
+function init_params()
+   params:add_number('midi_dev', "midi dev", 1, 16, 3)
+   params:set_action('midi_dev', function(v) midi_dev=midi.connect(v) end)
+   params:add_number('midi_ch', "midi ch", 1, 16)
+
+   params:add_number('nibble', "nibble", 0, 2^4-1, 4)
+   params:set_action('nibble', function() giggle:settable(nibbleToParadiddle(params:get('nibble'))) end)
+
+   params:add_number('0', "sound 0", 0, 127, 63)
+   params:add_number('1', "sound 1", 0, 127, 64)
+end
+
+-- From https://stackoverflow.com/a/9080080
+function toBits(num, bits)
+   -- returns a table of bits, most significant first.
+   -- local bits = bits or math.max(1, select(2, math.frexp(num)))
+   local bits = bits or 16
+   local t = {} -- will contain the bits
+   for b = bits, 1, -1 do
+      t[b] = math.fmod(num, 2)
+      num = math.floor((num - t[b]) / 2)
+   end
+   return t
+end
+
+function toNibbleBits(num)
+   return toBits(num, 4)
+end
+
+local function invertNibble(num)
+   local bits = toNibbleBits(num)
+   local t = {}
+   for b = 1, 4 do
+      t[b] = 1 + bits[b] * -1
+   end
+   return t
+end
+
+function nibbleToParadiddle(num)
+   local p = {}
+   local bits = toNibbleBits(num)
+   local ibits = invertNibble(num)
+   for b=1,4 do
+      p[b] = bits[b]
+   end
+   for b=1,4 do
+      p[b+4] = ibits[b]
+   end
+   return p
+end
+
+function tick()
+   while true do
+      clock.sync(1/4)
+      current_value = giggle()
+      accent = accents()
+      if accent then
+        vel = 127
+      else
+        vel = 100
+      end
+      if current_value == 0 then
+         midi_dev:note_on(params:get('0'), vel, params:get('midi_ch'))
+      else
+         midi_dev:note_on(params:get('1'), vel, params:get('midi_ch'))
+      end
+      redraw()
+   end
+end
+
+function redraw()
+   screen.clear()
+   draw_nibble()
+   draw_music()
+   screen.update()
+end
+
+function draw_nibble()
+  local bitstring=""
+   local bits = nibbleToParadiddle(params:get('nibble'))
+   for b=1,8 do
+      bitstring=bitstring..bits[b]
+   end
+   screen.move(10, 10)
+   screen.level(2)
+   screen.font_size(8)
+   screen.text(bitstring)
+end
+
+function draw_music()
+   -- screen.move(screen.width/2, screen.height/2-(screen.height/4)*2*current_value)
+   screen.move(screen.width/2, screen.height - screen.height*0.8*current_value)
+   screen.level(10)
+   if accent then
+     screen.font_size(20)
+   else
+     screen.font_size(15)
+   end
+   screen.text('*')
+end
